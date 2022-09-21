78227e8f669a1a8bae1c3189f467424712a4884a
Priscila <prisciladaniela25@hotmail.com>
2022-09-21 11:04:43 -0300

    refactorizacion

--- file_handling/all_together.py
+++ file_handling/all_together.py
@@ -1,8 +1,18 @@
 
 print("Bienvenido al programa")
 user_input = ""
+def write_file():
+    file = open("file_handling/demo_two.txt", "a")
+    user_content = input("Ingresa el contenido\n")
+    file.write(user_content + "\n")
+    file.close()
 
-while user_input != "exit":
+def read_file():
+    file = open ("file_handling/demo_two.txt", "r" )
+    for line in file:
+          print(line)
+
+def print_menu():
   print("##################")
   print("ingresa una opción")
   print("1", "agregar contenido")
@@ -10,33 +20,17 @@ while user_input != "exit":
   print("exit", "para salir")
   print("##################")
 
+while user_input != "exit":
+  print_menu()
+  
   user_input = input()
 
   if user_input == "1":
-    file = open("file_handling/demo_two.txt", "a")
-    user_content = input("Ingresa el contenido\n")
-    file.write(user_content + "\n")
-    file.close()
+   write_file()
   elif user_input == "2":
-    file = open("file_handling/demo_two.txt", "r")
-    for line in file:
-      print(line)
+   read_file()
+  elif user_input == "exit":
+    print("chau chau")
+    exit()
   else:
     print("opción incorrecta")
-
-print("Chau chau")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          
